9e725764e5bede7e492d36ef5b00e00c5846fb8f
sjsj7434 <57528375+sjsj7434@users.noreply.github.com>
2023-03-28 17:11:17 +0900

    ckeditor5 추가
    기본 언어는 한국어

--- client-react/src/pages/MyEditor.js
+++ client-react/src/pages/MyEditor.js
@@ -1,20 +1,165 @@
 import { CKEditor } from '@ckeditor/ckeditor5-react';
-import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import ClassicEditor from 'ckeditor5-custom-build/build/ckeditor'; //online-build, custom build, [npm add file]
+// https://stackoverflow.com/questions/62243323/reactjs-import-ckeditor-5-from-online-build
+
+const editorConfiguration = {
+	toolbar: [
+		'heading',
+		'|',
+		'alignment',
+		'bold',
+		'italic',
+		'underLine',
+		'strikeThrough',
+		'horizontalLine',
+		'|',
+		'fontColor',
+		'fontFamily',
+		'fontSize',
+		'fontBackgroundColor',
+		'|',
+		'indent',
+		'outdent',
+		'numberedList',
+		'bulletedList',
+		'todoList',
+		'blockQuote',
+		'|',
+		'link',
+		// 'imageUpload',
+		'imageInsert',
+		'mediaEmbed',
+		'insertTable',
+		'|',
+		'findAndReplace',
+		'undo',
+		'redo',
+	],
+	fontFamily: {
+		options: [
+			'default',
+			'Arial',
+			'Georgia',
+			'궁서체',
+			'바탕',
+			'돋움'
+		],
+		supportAllValues: true
+	},
+	fontColor: {
+		colors: [
+			{
+				color: '#000000',
+				label: 'Black',
+				hasBorder: true
+			},
+			{
+				color: '#737373',
+				label: 'Dim grey',
+				hasBorder: true
+			},
+			{
+				color: '#b5b5b5',
+				label: 'Grey',
+				hasBorder: true
+			},
+			{
+				color: '#ebebeb',
+				label: '연한 회색',
+				hasBorder: true
+			},
+			{
+				color: '#ffffff',
+				label: 'White',
+				hasBorder: true
+			},
+			{
+				color: '#ff0000',
+				label: '붉은색',
+				hasBorder: true
+			},
+			{
+				color: '#FF8000',
+				label: '주황색',
+				hasBorder: true
+			},
+			{
+				color: '#fff700',
+				label: '노란색',
+				hasBorder: true
+			},
+			{
+				color: '#4de64d',
+				label: '초록색',
+				hasBorder: true
+			},
+			{
+				color: '#0022FF',
+				label: '파란색',
+				hasBorder: true
+			},
+			{
+				color: '#6600CC',
+				label: '보라색',
+				hasBorder: true
+			},
+			{
+				color: '#ff4e4e',
+				label: '연한 붉은색',
+				hasBorder: true
+			},
+			{
+				color: '#FF99CC',
+				label: '분홍색',
+				hasBorder: true
+			},
+			{
+				color: '#32D7FF',
+				label: '하늘색',
+				hasBorder: true
+			},
+			{
+				color: '#21ffcc',
+				label: '민트색',
+				hasBorder: true
+			},
+		]
+	},
+	wordCount: {
+		onUpdate: stats => {
+			// Prints the current content statistics.
+			console.log( `Characters: ${ stats.characters }\nWords: ${ stats.words }` );
+			const wordCountWrapper = document.getElementById( 'word-count' );
+			wordCountWrapper.textContent = `글자 수 => ${stats.characters}`;
+			wordCountWrapper.classList.toggle( 'demo-update__limit-close', false )
+		}
+	}
+};
 
 const MyEditor = () => {
 	return (
 		<>
 			<h2>Using CKEditor 5 build in React</h2>
 			<CKEditor
+				config={ editorConfiguration }
 				editor={ ClassicEditor }
 				data="<p>Hello from CKEditor 5!</p>"
 				onReady={ editor => {
 					// You can store the "editor" and use when it is needed.
 					console.log( 'Editor is ready to use!', editor );
+					// console.log(Array.from(editor.ui.componentFactory.names()).join(", "));
+
+					const wordCountPlugin = editor.plugins.get( 'WordCount' );
+					const wordCountWrapper = document.getElementById( 'word-count' );
+					// wordCountWrapper.appendChild( wordCountPlugin.wordCountContainer ); //기본 view
 				} }
 				onChange={ ( event, editor ) => {
 					const data = editor.getData();
-					console.log( { event, editor, data } );
+					// console.log( { event, editor, data } );
+					console.log(data);
+					const wordCountWrapper = document.getElementById( 'html-count' );
+					wordCountWrapper.textContent = `html 글자 수 => ${data.length}`;
+					wordCountWrapper.classList.toggle( 'demo-update__limit-close', false )
 				} }
 				onBlur={ ( event, editor ) => {
 					console.log( 'Blur.', editor );
@@ -23,6 +168,8 @@ const MyEditor = () => {
 					console.log( 'Focus.', editor );
 				} }
 			/>
+			<div id="word-count"></div>
+			<div id="html-count"></div>
 		</>
 	);
 }
