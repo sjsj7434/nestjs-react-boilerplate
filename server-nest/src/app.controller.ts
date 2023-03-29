ec68a756bcbbaceeb2d8771ef3e261ee7602957f
sjsj7434 <57528375+sjsj7434@users.noreply.github.com>
2023-03-29 15:19:51 +0900

    upload file random name

--- server-nest/src/app.controller.ts
+++ server-nest/src/app.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { AppService } from './app.service';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { extname } from 'path';
 
 @Controller()
 export class AppController {
@@ -14,8 +15,10 @@ export class AppController {
 
 	@Post('image')
 	@UseInterceptors(FileInterceptor('upload'))
-	uploadFile(@UploadedFile() file) {
-		console.log('[server console] uploadFile');
+	uploadFile(@UploadedFile() file: Express.Multer.File): Object {
+		//Multer is --save-dev option installed
+		const randomName = Array(32).fill(null).map(() => (Math.round(Math.random() * 16)).toString(16)).join('');
+		console.log('[server console] uploadFile', randomName, extname(file.originalname));
 		console.log(file);
 		return { "url": "https://img.megastudy.net/campus/library/v2015/library/intro_renew/main_top_banner_teamplay_gate_221216.jpg" };
 	}
