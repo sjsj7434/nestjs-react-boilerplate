71b5e038fedfa1e4051eb74b45987dc5ebb23fee
sjsj7434 <57528375+sjsj7434@users.noreply.github.com>
2023-03-29 14:01:02 +0900

    post image 받는 필드 이름 변경

--- server-nest/src/app.controller.ts
+++ server-nest/src/app.controller.ts
@@ -6,15 +6,17 @@ import { FileInterceptor } from '@nestjs/platform-express';
 export class AppController {
 	constructor(private readonly appService: AppService) {}
 
-	@Get('api/users')
-	getHello(): string {
-		return this.appService.getHello();
+	@Get('hello')
+	getHello(): Object {
+		console.log('[server console] getHello');
+		return { "message": this.appService.getHello() };
 	}
 
 	@Post('image')
-	@UseInterceptors(FileInterceptor('file'))
+	@UseInterceptors(FileInterceptor('upload'))
 	uploadFile(@UploadedFile() file) {
-		console.log('uploadFile', file);
+		console.log('[server console] uploadFile');
+		console.log(file);
 		return { "url": "https://img.megastudy.net/campus/library/v2015/library/intro_renew/main_top_banner_teamplay_gate_221216.jpg" };
 	}
 }
